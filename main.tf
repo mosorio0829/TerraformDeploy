df4457dffe912ce909fd3d6e273835411291d66c
Manuel <aor.manuel@gmail.com>
2025-02-13 00:10:47 +0000

    GH Action test

--- main.tf
+++ main.tf
@@ -2,16 +2,20 @@ terraform {
   required_version = ">=1.3.0"
   required_providers {
     azurerm = {
-      "source" = "hashicorp/azurerm"
-      version  = "3.43.0"
+      source  = "hashicorp/azurerm"
+      version = "3.43.0"
     }
   }
 }
 
-
 provider "azurerm" {
   features {}
-  skip_provider_registration = true
+
+  # Authentication using Service Principal
+  client_id       = var.azure_client_id       # Variable for Client ID
+  client_secret   = var.azure_client_secret     # Variable for Client Secret
+  tenant_id       = var.azure_tenant_id         # Variable for Tenant ID
+  subscription_id  = var.azure_subscription_id   # Variable for Subscription ID
 }
 
 resource "random_string" "uniquestring" {
